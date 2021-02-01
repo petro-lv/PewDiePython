9b68d74a251ea9535075cea9753cd799b161c8fb
petro-lv <psidlyar70@gmail.com>
2021-02-01 21:20:37 +0200

    analyzing photos

---
+++ pewdiepython.py
@@ -0,0 +1,76 @@
+
+import instaloader
+
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+
+subscription_key = ""
+endpoint = ""
+
+computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+class User:
+
+    def __init__(self, username):
+
+        instance = instaloader.Instaloader()
+
+        self.username = username
+
+        posts = instaloader.Profile.from_username(instance.context, self.username).get_posts()
+
+        self.photos = [i.url for i in posts] #Список посилань на фотографії
+
+        #Потрібно логінитись (юзернейм, пароль)
+        # self.following = [i for i in instaloader.Profile.from_username(instance.context, self.username).get_followees()] #Cписок юзернеймів популярних людей
+
+    #Аналіз фотографій користувача(не виникає помилок із відео та багатьма фотографіями)
+    def analyze(self):
+
+        trending_colors = []
+        remote_image_features = ["color"]
+
+        for i in range(3):
+            remote_image_url = self.photos[i]
+            detect_color_results_remote = computervision_client.analyze_image(remote_image_url, remote_image_features)
+            trending_colors.append([detect_color_results_remote.color.dominant_color_foreground, f'{i+1} фотографія', remote_image_url])
+
+        return trending_colors
+
+    #Аналіз фотографій популярних людей, на яких підписаний користувач
+    def analyze_top(self):
+
+        top_trending_colors = []
+
+        # for i in self.following:
+        #     top = User(i)
+        #     top_trending_colors.append(top.analyze_user())
+
+        return top_trending_colors
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
