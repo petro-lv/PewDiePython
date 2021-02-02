5c7aafc1331cd82fc2eaa9b0e5842e900db14ae2
Drru97 <oleg.dystak@gmail.com>
2021-02-02 14:45:42 +0200

    Clean-up.

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 from flask import Flask, request, jsonify
-from pewdiepython import User
+from pewdiepython import User, User_logged
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+app.config['DEBUG'] = True
+
+instagramUsername = 'username'
+instagramUsernameHeader = 'Insta-Username'
+instagramPasswordHeader = 'Insta-Password'
 
 
-@app.route('/analyze', methods=['GET'])
+@app.route('/api/instagram/analyze', methods=['GET'])
 def analyze():
-    if 'username' in request.args:
-        username = str(request.args['username'])
+    if instagramUsername in request.args:
+        username = str(request.args[instagramUsername])
+
         user = User(username)
         colors = user.analyze()
+
         return jsonify(colors)
     else:
-        return "Error"
+        return "Error", 400
+
+
+@app.route('api/instagram/analyze-top', methods=['GET'])
+def analyze_following():
+    if instagramUsernameHeader and instagramPasswordHeader in request.headers:
+        username = str(request.headers.get(instagramUsernameHeader))
+        password = str(request.headers.get(instagramPasswordHeader))
+
+        user = User_logged(username, password)
+        colors = user.analyze_top()
+
+        return jsonify(colors)
+    else:
+        return "Error", 400
