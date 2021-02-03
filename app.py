16f56020c80a74ca46bfb2a6fd886ded1117a715
Drru97 <oleg.dystak@gmail.com>
2021-02-03 17:40:01 +0200

    Add CORS policy.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 from pewdiepython import User, User_logged, ResultEncoder
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 app.json_encoder = ResultEncoder
+
+cors = CORS(app)
 
 instagramUsername = 'username'
 instagramUsernameHeader = 'Insta-Username'
@@ -11,6 +14,7 @@ instagramPasswordHeader = 'Insta-Password'
 
 
 @app.route('/api/instagram/analyze', methods=['GET'])
+@cross_origin()
 def analyze():
     if instagramUsername in request.args:
         username = str(request.args[instagramUsername])
@@ -24,6 +28,7 @@ def analyze():
 
 
 @app.route('/api/instagram/analyze-top', methods=['GET'])
+@cross_origin()
 def analyze_following():
     if instagramUsernameHeader and instagramPasswordHeader in request.headers:
         username = str(request.headers.get(instagramUsernameHeader))
